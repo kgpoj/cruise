302c02ffb48a7a057217746bdf538e0b1580a459
zty <15010972518@163.com>
2023-03-22 11:16:51 +0800

    feat: change device type context when resizing

--- src/store/CruiseContextProvider.tsx
+++ src/store/CruiseContextProvider.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import { Layout } from 'antd';
 import DeviceContext from './DeviceContext';
@@ -6,12 +6,34 @@ import DeviceContext from './DeviceContext';
 const StyledLayout = styled(Layout)`
   height: 100vh;
 `;
-const CruiseContextProvider = ({ children }: { children: React.ReactNode }) => (
-  <StyledLayout>
-    <DeviceContext.Provider value={window.innerWidth < 992 ? 'mobile' : 'desktop'}>
-      {children}
-    </DeviceContext.Provider>
-  </StyledLayout>
-);
+const CruiseContextProvider = ({ children }: { children: React.ReactNode }) => {
+  const [deviceType, setDeviceType] = useState('');
+
+  useEffect(() => {
+    const handleResize = () => {
+      const width = window.innerWidth;
+      if (width >= 992) {
+        setDeviceType('desktop');
+      } else if (width >= 768) {
+        setDeviceType('tablet');
+      } else {
+        setDeviceType('mobile');
+      }
+    };
+
+    handleResize();
+    window.addEventListener('resize', handleResize);
+
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
+  return (
+    <StyledLayout>
+      <DeviceContext.Provider value={deviceType}>
+        {children}
+      </DeviceContext.Provider>
+    </StyledLayout>
+  );
+};
 
 export default CruiseContextProvider;
