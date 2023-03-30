0a21de72823c6b71544183df6c877cd19ac0169c
zty <15010972518@163.com>
2023-03-30 11:25:45 +0800

    refactor: split AgentTypeBox tests

--- src/pages/Agent/components/AgentTypeBox/index.test.tsx
+++ src/pages/Agent/components/AgentTypeBox/index.test.tsx
@@ -3,15 +3,27 @@ import { render, screen } from '@testing-library/react';
 import AgentTypeBox from './index';
 
 describe('AgentTypeBox', () => {
-  it('should render correctly', () => {
-    const physicalCount = 4;
-    const virtualCount = 5;
+  const physicalCount = 4;
+  const virtualCount = 5;
+
+  it('should render ALL correctly', () => {
     render(<AgentTypeBox physicalCount={physicalCount} virtualCount={virtualCount} />);
+
     expect(screen.getByText('ALL')).toBeInTheDocument();
-    expect(screen.getByText('PHYSICAL')).toBeInTheDocument();
-    expect(screen.getByText('VIRTUAL')).toBeInTheDocument();
-    expect(screen.getByText(physicalCount)).toBeInTheDocument();
-    expect(screen.getByText(virtualCount)).toBeInTheDocument();
     expect(screen.getByText(physicalCount + virtualCount)).toBeInTheDocument();
+  });
+
+  it('should render PHYSICAL correctly', () => {
+    render(<AgentTypeBox physicalCount={physicalCount} virtualCount={virtualCount} />);
+
+    expect(screen.getByText('PHYSICAL')).toBeInTheDocument();
+    expect(screen.getByText(physicalCount)).toBeInTheDocument();
+  });
+
+  it('should render VIRTUAL correctly', () => {
+    render(<AgentTypeBox physicalCount={physicalCount} virtualCount={virtualCount} />);
+
+    expect(screen.getByText('VIRTUAL')).toBeInTheDocument();
+    expect(screen.getByText(virtualCount)).toBeInTheDocument();
   });
 });
