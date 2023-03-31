3bb47acfa86f2d1991187064a15e8927069fe25c
zty <15010972518@163.com>
2023-03-31 09:38:41 +0800

    feat: add Agent interface

--- src/interface/Agent.ts
+++ src/interface/Agent.ts
@@ -1,1 +1,20 @@
 export type Availability = 'building' | 'idle';
+
+export type AgentType = 'physical' | 'virtual';
+
+export type ResourceName = 'Firefox' | 'Chrome' | 'Safari' | 'Ubuntu';
+
+export interface Resource {
+  id: string;
+  name: ResourceName;
+}
+
+export interface Agent {
+  id: string;
+  name: string;
+  iconUrl: string;
+  ipAddress: string;
+  availability: Availability;
+  agentType: AgentType;
+  resources: Resource[];
+}
