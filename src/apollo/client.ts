b99375691d934cb4f3b59be4a5a17382d1fdffc9
zty <15010972518@163.com>
2023-05-19 14:39:08 +0800

    change production ip

--- src/apollo/client.ts
+++ src/apollo/client.ts
@@ -1,6 +1,6 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
-const PRODUCTION_API = 'http://ec2-3-26-180-216.ap-southeast-2.compute.amazonaws.com:3001/graphql';
+const PRODUCTION_API = 'http://ec2-13-236-66-159.ap-southeast-2.compute.amazonaws.com:3001/graphql';
 const DEVELOPMENT_API = 'http://localhost:3001/graphql';
 const CRUISE_API = process.env.NODE_ENV === 'development' ? DEVELOPMENT_API : PRODUCTION_API;
 console.log(process.env.NODE_ENV);
