9c05117829a1711253db0b53260ee485af5d9099
zty <15010972518@163.com>
2023-05-23 11:18:52 +0800

    ignore lint porblems

--- Jenkinsfile
+++ Jenkinsfile
@@ -7,6 +7,7 @@ pipeline {
         stage('Test') {
             steps {
                 sh 'npm install'
+                sh 'npm run lint'
                 sh 'npm run test'
             }
         }
--- src/apollo/client.ts
+++ src/apollo/client.ts
@@ -3,6 +3,7 @@ import { ApolloClient, InMemoryCache } from '@apollo/client';
 const PRODUCTION_API = 'http://ec2-13-236-66-159.ap-southeast-2.compute.amazonaws.com:3001/graphql';
 const DEVELOPMENT_API = 'http://localhost:3001/graphql';
 const CRUISE_API = process.env.NODE_ENV === 'development' ? DEVELOPMENT_API : PRODUCTION_API;
+// eslint-disable-next-line no-console
 console.log(process.env.NODE_ENV);
 const client = new ApolloClient({
   uri: CRUISE_API,
